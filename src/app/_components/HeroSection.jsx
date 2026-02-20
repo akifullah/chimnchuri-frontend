45889d4f6e86dff7c11387864666305c58380115
Akif Ullah <akifullah0317@gmail.com>
2026-02-20 09:43:58 +0500

    Update hero section

--- src/app/_components/HeroSection.jsx
+++ src/app/_components/HeroSection.jsx
@@ -1,36 +1,26 @@
 import React from 'react'
+import c from "@/assets/images/c.png"
+import h from "@/assets/images/h.png"
+import i from "@/assets/images/i.png"
+import m from "@/assets/images/m.png"
+import n from "@/assets/images/n.png"
+import u from "@/assets/images/u.png"
+import r from "@/assets/images/r.png"
 
 const HeroSection = () => {
+    let images = [{ src: c, alt: "C" }, { src: h, alt: "H" }, { src: i, alt: "I" }, { src: m, alt: "M" }, { src: n, alt: "N" }, { src: c, alt: "C" }, { src: h, alt: "H" }, { src: u, alt: "U" }, { src: r, alt: "R" }, { src: r, alt: "R" }, { src: i, alt: "I" }];
     return (
         <>
             <section>
                 <div className="bg-brand py-10 min-h-[40vh] lg:min-h-[70vh] flex items-center justify-center">
                     <div className="flex justify-center">
-                        <img src="./c.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${0 * 0.12}s` }} />
-                        <img src="./h.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${1 * 0.12}s` }} />
-                        <img src="./i.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${2 * 0.12}s` }} />
-                        <img src="./m.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${3 * 0.12}s` }} />
-                        <img src="./n.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${4 * 0.12}s` }} />
-                        <img src="./c.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${5 * 0.12}s` }} />
-                        <img src="./h.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${6 * 0.12}s` }} />
-                        <img src="./u.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${7 * 0.12}s` }} />
-                        <img src="./r.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${8 * 0.12}s` }} />
-                        <img src="./r.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${9 * 0.12}s` }} />
-                        <img src="./i.png" alt="" height={100} className="h-[12vw] animate-image-wave"
-                            style={{ animationDelay: `${10 * 0.12}s` }} />
+                        {
+                            images?.map((img, index) => (
+                                <img key={index} src={img?.src?.src} alt={img.alt} height={100} className="h-[12vw] animate-image-wave"
+                                    style={{ animationDelay: `${index * 0.12}s` }} />
+                            ))
+                        }
                     </div>
-
-                    {/* <h1 className="text-white text-[13vw] font-bold uppercase text-center text-shadow-lg">Chimnchurri</h1> */}
                 </div>
             </section>
         </>
