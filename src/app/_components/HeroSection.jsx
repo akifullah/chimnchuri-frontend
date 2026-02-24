110281761e8e21ff9981bafa8c3ea9cc92c3be23
khyberwear <atifjan2019@gmail.com>
2026-02-24 11:41:31 +0000

    style: Refined 'Order Online' button padding and display in header.

--- src/app/_components/HeroSection.jsx
+++ src/app/_components/HeroSection.jsx
@@ -13,26 +13,7 @@ const HeroSection = () => {
     const settings = useSettings();
 
     // let images = [{ src: c, alt: "C" }, { src: h, alt: "H" }, { src: i, alt: "I" }, { src: m, alt: "M" }, { src: n, alt: "N" }, { src: c, alt: "C" }, { src: h, alt: "H" }, { src: u, alt: "U" }, { src: r, alt: "R" }, { src: r, alt: "R" }, { src: i, alt: "I" }];
-    return (
-        <>
-            <section>
-                <div className="bg-brand py-0 min-h-[10vh] lg:min-h-[50vh] flex items-center justify-center">
-                    <div className="flex justify-center">
-
-                        <img src={settings?.restaurant_logo ? `${process.env.NEXT_PUBLIC_BASE_URL}/${settings?.restaurant_logo}` : "/logo-light.png"} className='max-w-[80vw]  sm:max-w-[50vw] md:max-w-[30vw] mx-auto' onError={(e) => e.target.src = "/logo-light.png"} alt="Chim 'N' Churri " />
-
-
-                        {/* {
-                            images?.map((img, index) => (
-                                <img key={index} src={img?.src?.src} alt={img.alt} height={100} className="h-[12vw] animate-image-wave"
-                                    style={{ animationDelay: `${index * 0.12}s` }} />
-                            ))
-                        } */}
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+    return null
 }
 
 export default HeroSection
